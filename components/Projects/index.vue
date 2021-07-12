0fdec83e6559ada108d2704d1ffe76952d96136d
Ibrahim Abdullahi Aliyu <Ibrahiimaa05@gmail.com>
2021-07-12 08:03:02 +0100

    Change text

--- components/Projects/index.vue
+++ components/Projects/index.vue
@@ -45,7 +45,7 @@
                     {title:'Lissafi', link: 'https://app.lissafi.com', summary: 'Accounting software for small & medium businesses', description: `Building an accounting software for small and medium scale businesses, including accessibility features for people with little educational background.`, icons: ['icon-django', 'icon-vue-dot-js', 'icon-amazonaws', 'icon-mysql', 'icon-node-dot-js', 'icon-flask']},
                     {title:'Remindo', link: 'https://remindo.netlify.app', summary: 'An interactive task management software', description: 'Building an task management(todo-like) software with the power of PWA and Notification API', icons: ['icon-vue-dot-js', 'icon-couchdb-icon', 'icon-googlecloud' ]},
                     {title:'Pipersoft', link: 'https://piper.com.ng', summary: 'Bookkeeping & Inventory management software', description: 'Testing software and reports back issues and bugs to improve software performance', icons: ['icon-django', 'icon-vue-dot-js', 'icon-couchdb-icon']},
-                    {title:' JSON { BIN } IT!', link: 'https://jsonbin.it', summary: 'A new way to store JSON on the fly', description: 'People have preferences when it comes to choosing theme when using an app. I implemented a dark theme for the app to allow that flexibility.', icons: ['icon-django', 'icon-vue-dot-js', 'icon-couchdb-icon'], oss: true},
+                    {title:' JSON { BIN } IT!', link: 'https://jsonbin.it', summary: 'A new way to store JSON on the fly', description: 'Adds dark theme to the app and makes it possible for users to toggle between light and dark theme.', icons: ['icon-django', 'icon-vue-dot-js', 'icon-couchdb-icon'], oss: true},
                     
                 ]
             }
